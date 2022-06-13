1ee939b4b2808147d4359e1d5e218878d8451391
Emiliano Cassoni <cassoniemiliano@gmail.com>
2022-06-13 15:33:59 -0300

    Update environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://api.themoviedb.org/3',
+  apiKey: '8e7c93deb4eb878aad980af70d04c4b1',
+  images: 'https://image.tmdb.org/t/p',
 };
