cd3c867fd75edd920d9ca679362d89a3ef6c33c5
Emiliano Cassoni <cassoniemiliano@gmail.com>
2022-06-13 14:48:18 -0300

    Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: 'https://api.themoviedb.org/3',
   apiKey: '8e7c93deb4eb878aad980af70d04c4b1',
   images: 'https://image.tmdb.org/t/p',
